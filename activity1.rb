b9cf8f57707cc2ca1695be2d0fbbf5a99c0162c1
Gene Patrick Ray Alvarez <genepatrickalvarez@gmail.com>
2022-09-19 10:47:07 +0800

    Solutions for activity 1

--- activity1.rb
+++ activity1.rb
@@ -1,4 +1,36 @@
-puts "Hello World"
-puts "new line"
-puts "new line again"
-puts "new test?"
+puts "Activity 1"
+
+puts "Solution for number 1"
+[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].each { |n| puts "#{n}" }
+
+puts "\n Soulution for number 2"
+
+h = {a:1, b:2, c:3, d:4}
+
+puts "#{h[:b]}"
+
+new_key = :e
+new_value = 5
+h[new_key] = new_value
+
+puts h
+
+puts "\n Solution for number 3"
+
+contact_data = [["john@email.com", "123 Main st.", "555-123-4567"],
+            ["avion@email.com", "404 Not Found Dr.", "123-234-3454"]]
+
+contacts = {"John Cruz" => {}, "Avion School" => {}}
+
+contacts = {
+    "John Cruz" => { :email => contact_data[0][0], :address => contact_data[0][1], :phone => contact_data[0][2] },
+    "Avion School" => {:email => contact_data[1][0], :address => contact_data[1][1], :phone => contact_data[1][2] }
+}
+
+puts contacts
+
+# Expected output:
+#  {
+#    "John Cruz"=>{:email=>"john@email.com", :address=>"123 Main st.", :phone=>"555-123-4567"},
+#    "Avion School"=>{:email=>"avion@email.com", :address=>"404 Not Found Dr.",  :phone=>"123-234-3454"}
+#  }
---
+++ age.rb
@@ -0,0 +1,17 @@
+puts "\n Solution for number 4"
+
+puts "How old are you?"
+
+age = gets.to_i
+
+puts "In 10 years, you will be: \n"
+puts age + 10
+
+puts "In 20 years, you will be: \n"
+puts age + 20
+
+puts "In 30 years, you will be: \n"
+puts age + 30
+
+puts "In 40 years, you will be: \n"
+puts age + 40
