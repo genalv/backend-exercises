6c434e266e236dab5821d5e7ecaa61b26ec02375
Gene Patrick Ray Alvarez <genepatrickalvarez@gmail.com>
2022-10-03 19:47:18 +0800

    activity unique_in_order comment

--- activity11/activity_uniqueinorder.rb
+++ activity11/activity_uniqueinorder.rb
@@ -7,9 +7,9 @@ def unique_in_order(sample)
             unique << sample[i]
         end
     end
-    puts "#{unique}"
+    unique
 end
 
-unique_in_order('AAAABBBCCDAABBB') #["A", "B", "C", "D", "A", "B"]
-unique_in_order('ABBCcAD') #["A", "B", "C", "c", "A", "D"]
-unique_in_order([1,2,2,3,3]) #[1, 2, 3]
+p unique_in_order('AAAABBBCCDAABBB') #["A", "B", "C", "D", "A", "B"]
+p unique_in_order('ABBCcAD') #["A", "B", "C", "c", "A", "D"]
+p unique_in_order([1,2,2,3,3]) #[1, 2, 3]
